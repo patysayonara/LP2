7df162ea5c1919ff97650bb59415502d5ace8721
Patricia Sayonara Goes de Araujo <patysayonara@A202-15L.imd.local>
2018-09-03 16:50:16 -0300

    Terminando Q02 com alguns casos testes na Main.

--- Exercicios04/Q02/Deposito.java
+++ Exercicios04/Q02/Deposito.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Deposito extends Produto{
 	private ArrayList<Produto> storage;
+	private Scanner scan;
 
 	public Deposito(){
-		storage = new ArrayList<Produto>;
+		storage = new ArrayList<Produto>();
+		scan = new Scanner(System.in);
 	}
 
 	public void addProduto(Produto produto){
@@ -12,7 +15,18 @@ public class Deposito extends Produto{
 	}	
 
 	public void rmProduto(){
-		
+		System.out.println("These are the products in storage:");
+		for(Produto item: storage){
+			System.out.println(storage.indexOf(item) + " - " + item.getNome());
+		}	
+		System.out.println("Please inform the number that corresponds to the item of choice:");
+		System.out.print("> ");
+		int toBeDeleted = scan.nextInt();
+		if(toBeDeleted < 0 || toBeDeleted >= storage.size()){
+			System.out.println("Invalid number!");
+		} else {
+			storage.remove(toBeDeleted);
+		}
 	}
 
 	public int getNumProdutos(){
@@ -27,7 +41,15 @@ public class Deposito extends Produto{
 		}
 	}
 
-	public String biggerValue(){
-	
+	public void highestValue(){
+		double highest = 0.00;
+		int index = 0;
+		for(Produto item: storage){
+			if(item.getPreco() > highest){
+				highest = item.getPreco();
+				index = storage.indexOf(item);
+			}
+		}
+		System.out.println("Product with the highest value is: " + storage.get(index).getNome());
 	}
 }
--- Exercicios04/Q02/Livro.java
+++ Exercicios04/Q02/Livro.java
@@ -4,21 +4,15 @@ public class Livro extends ProdutoDuravel{
 
 	public Livro(){
 		autor = "Unknown";
-		editora = "Unknown";
 	}
 
-	public Livro(String nome, double preco, String marca, String descricao, String dataFabricacao, String materialPredominante, String durabilidade, String autor, String editora){
+	public Livro(String nome, double preco, String marca, String descricao, String dataFabricacao, String materialPredominante, String durabilidade, String autor){
 		super(nome, preco, marca, descricao, dataFabricacao, materialPredominante, durabilidade);
 		this.autor = autor;
-		this.editora = editora;
 	}
 
 	public String getAutor(){
 		return autor;
-	}
-
-	public String getEditora(){
-		return editora;
 	}
 
 }
